189f6658cd427dcfffde971d3a2f6cecf6489813
Panos Orovas <panagiwtis.orovas@gmail.com>
2026-02-23 00:48:59 +0200

    minor ui style updates

--- apps/web/components/message.tsx
+++ apps/web/components/message.tsx
@@ -99,7 +99,7 @@ const PurePreviewMessage = ({
 
         <div
           className={cn('flex flex-col', {
-            'gap-2 md:gap-4': message.parts?.some(
+            'gap-2': message.parts?.some(
               (p) => p.type === 'text' && p.text?.trim()
             ),
             'w-full':
